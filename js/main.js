72f4e9a730a0d492c053c6a228c0e55b1b5fde77
Josiel Barbosa Nascimento <BarbosaNascimentoJosiel@JohnDeere.com>
2020-11-11 15:15:28 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*
+alert("Meu primeiro JS");
+var lista = ["maca", "uva", "pera"];
+console.log(lista.join(" - "));*/
+
+/*var idade = 18;
+
+if (idade >= 18) {
+  alert("Maior de idade");
+} else {
+  alert("Menor de idade");
+}*/
